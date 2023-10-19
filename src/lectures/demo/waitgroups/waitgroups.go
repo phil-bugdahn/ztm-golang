98e5716239761ffcdcf44bce2c2d29a5b1eb61c3
Phil Bugdahn <phil.bugdahn@outlook.com>
2023-10-19 08:04:42 -0400

    WIP

--- src/lectures/demo/waitgroups/waitgroups.go
+++ src/lectures/demo/waitgroups/waitgroups.go
@@ -8,4 +8,25 @@ import (
 )
 
 func main() {
+    rand.Seed(time.Now().UnixNano())
+
+    var wg sync.WaitGroup
+    counter := 0
+    for i := 0; i < 5; i++ {
+        wg.Add(1)
+        counter += 1
+        go func() {
+            defer func() {
+                fmt.Println(counter, "goroutines remaining")
+                counter -= 1
+                wg.Done()
+            }()
+            duration := time.Duration(rand.Intn(500) * int(time.Millisecond))
+            fmt.Println("waitin for ", duration)
+            time.Sleep(duration)
+        }()
+    }
+    fmt.Println("wiaitng for goroutines")
+    wg.Wait()
+    fmt.Println("done!")
 }
