ca87ba4b3e32c8a5ced3b75077c56701bc2a4f4c
Phil Bugdahn <phil.bugdahn@outlook.com>
2023-12-21 15:56:56 -0500

    PROGRESS

--- src/lectures/demo/ctx/ctx.go
+++ src/lectures/demo/ctx/ctx.go
@@ -1,5 +1,52 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"time"
+)
 
+func sampleOperation(ctx context.Context, msg string, msDelay time.Duration) <-chan string {
+	out := make(chan string)
+
+	go func() {
+		for {
+			select {
+			case <-time.After(msDelay * time.Millisecond):
+				out <- fmt.Sprintf("%v operation completed", msg)
+				return
+			case <-ctx.Done():
+				out <- fmt.Sprintf("%v aborted", msg)
+				return
+			}
+		}
+	}()
+	return out
+}
+
+func main() {
+	ctx := context.Background()
+	// cancelCtx can cancel anything using ctx
+	ctx, cancelCtx := context.WithCancel(ctx)
+
+	webServer := sampleOperation(ctx, "webServer", 200)
+	microService := sampleOperation(ctx, "microservice", 500)
+	database := sampleOperation(ctx, "database", 900)
+
+	MainLoop:
+	for {
+		select {
+		case val := <-webServer:
+			fmt.Println(val)
+		case val := <-microService:
+			fmt.Println(val)
+			fmt.Println("Cancel context")
+			cancelCtx()
+			break MainLoop
+		case val := <-database:
+			fmt.Println(val)
+		}
+	}
+
+	fmt.Println(<-database)
 }
--- src/lectures/demo/generator/generator.go
+++ src/lectures/demo/generator/generator.go
@@ -1,4 +1,58 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func generateRandInt(min, max int) <-chan int {
+	out := make(chan int, 3)
+
+	go func() {
+		for {
+			out <- rand.Intn(max - min + 1) + min
+		}
+	}()
+	return out
+}
+
+func generateRandIntn(count, min, max int) <-chan int {
+	out := make(chan int, 1)
+	go func() {
+		for i := 0; i < count; i++ {
+			out <- rand.Intn(max - min + 1) + min
+		}
+		close(out)
+	}()
+
+	return out
+}
+
 func main() {
+	rand.Seed(time.Now().UnixNano())
+	randInt := generateRandInt(1, 100)
+
+	fmt.Println("genereateRandInt infinite")
+	fmt.Println(<-randInt)
+	fmt.Println(<-randInt)
+	fmt.Println(<-randInt)
+	fmt.Println(<-randInt)
+
+	randIntRange := generateRandIntn(2, 1, 10)
+
+	fmt.Println("genereateRandInt using finite count")
+	for i := range randIntRange {
+		fmt.Println(i)
+	}
+
+	fmt.Println("genereateRandInt using infinite loop")
+	randIntn := generateRandIntn(3, 1, 10)
+	for {
+		n, open := <-randIntn
+		if !open {
+			break
+		}
+		fmt.Println(n)
+	}
 }
